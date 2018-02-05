bfb2febc75fc74c1c18b9a54f7f27580479ea6f9
shenyifei <m809745357@gmail.com>
2018-02-05 23:39:05 +0800

    图片裁剪

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -42,20 +42,28 @@ class WeChatController extends Controller
         $app = app('wechat.official_account');
         $stream = $app->media->get($request->serverId);
 
-        $filename = $stream->save(public_path('uploads/fontend'));
+        // 构建存储的文件夹规则，值如：uploads/images/avatars/201709/21/
+        // 文件夹切割能让查找效率更高。
+        $folder_name = "uploads/images/$folder/" . date("Ym", time()) . '/'.date("d", time()).'/';
 
-        $this->reduceSize(public_path('uploads/fontend') . '/' . $file_path);
+        // 文件具体存储的物理路径，`public_path()` 获取的是 `public` 文件夹的物理路径。
+        // 值如：/home/vagrant/Code/larabbs/public/uploads/images/avatars/201709/21/
+        $upload_path = public_path() . '/' . $folder_name;
+
+        $filename = $stream->save($upload_path);
+
+        $this->cropSize($upload_path . '/' . $filename);
 
         return '/uploads/' . $filename;
     }
 
-    public function reduceSize($file_path)
+    public function cropSize($file_path)
     {
         // 先实例化，传参是文件的磁盘物理路径
         $image = Image::make($file_path);
 
         // 进行大小调整的操作
-        $image->resize(375, 300);
+        $image->crop(375, 300);
 
         // 对图片修改后进行保存
         $image->save();
